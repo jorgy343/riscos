543a49d796248a0497955e81eb2ab3cdaed16ec8
Alex Jorgenson <jorgy343@gmail.com>
2025-03-23 18:53:43 +0000

    Refactor kernel entry point and update linker script.
    - Moved the kernel entry point function definition to improve organization and clarity.
    - Updated the global assembly to reference the new kernel entry point.
    - Modified the linker script to include the kernel entry point in the text section for proper linking.

--- src/kernel/src/lib.rs
+++ src/kernel/src/lib.rs
@@ -2,9 +2,33 @@
 
 mod sbi;
 
-use core::panic::PanicInfo;
+use core::{arch::global_asm, panic::PanicInfo};
+
+#[unsafe(no_mangle)]
+pub extern "C" fn kernel_main() -> ! {
+    debug_println!("\nWelcome to the kernel!\n");
+
+    loop {}
+}
 
 #[panic_handler]
 fn panic(_panic: &PanicInfo) -> ! {
     loop {}
 }
+
+global_asm!(
+    "
+    .global _kernel_entrypoint
+
+    .extern kernel_main
+
+    .section .text.kernel_entrypoint
+    
+    _kernel_entrypoint:        
+        jal boot_main
+
+    infinite:   // Infinite loop if kernel_main returns.
+        wfi
+        j infinite
+    "
+);
